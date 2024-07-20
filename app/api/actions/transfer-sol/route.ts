1d57ee39acd17c9afee76fcabe5e17637f58d417
itsmeneiconel <neiconelgonzalesbandala@gmail.com>
2024-07-20 16:34:32 +0800

    240720 163428

--- app/api/actions/transfer-sol/route.ts
+++ app/api/actions/transfer-sol/route.ts
@@ -1,33 +1,19 @@
 import { ACTIONS_CORS_HEADERS, ActionGetResponse, ActionPostRequest, ActionPostResponse, createPostResponse } from "@solana/actions";
 import { transferSolTransaction } from "./transaction";
 
-const icon_ids = ['1lrmK3Rurt5NAY8-O_VwdkCErWbkisNZY','11HsuitWbpYGL0tcITHPo-_CjpUuDAFy6','1W8HPq9Xf2gXBzgRLqjJACtivYZH5rwdN']
-let icon_id = icon_ids;
-let count = 0;
-
 export const GET = async (req: Request) => {
     const payload: ActionGetResponse = {
         title: "SuperteamPHL 🇵🇭 Transfer SOL BLINK",
         // icon: "https://i.imgur.com/wKY2gEc.jpeg", // reference 
-        icon: `https://drive.usercontent.google.com/download?id=${icon_id[count]}`,
+        icon: `https://drive.google.com/uc?export=view&id=1TPmgCQSTVE5x1WWaXhKy1nRjtf8eX8YC`,
         description: "Transfer SOL to another wallet",
-        label: "Grow with SOL 🤙🏻🤙🏻🤙🏻"
+        label: "ADOPT ME CUTIE BOI"
     }
 
     return Response.json(payload, {
         headers: ACTIONS_CORS_HEADERS,
     });
 }
-
-// make a solana blink that updates its metadata whenever there's a successful transaction 
-//     metadata holds 
-//         url to icon
-//     whenever there's a successful transaction, updates the url to icon of next stage 
-//         url to icon stages
-//             normal
-//             abnormal
-//             super
-//         by default normal should be the icon
 
 export const OPTIONS = GET;
 
@@ -40,7 +26,6 @@ export const POST = async (req: Request) => {
             message: `Send 1 SOL`,
         },
     });
-    count++;
     return Response.json(payload, {
         headers: ACTIONS_CORS_HEADERS,
     });
--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,8 @@ const nextConfig = {
                 port: '',
                 pathname: '/**',
               },
-        ]
+        ],
+        domains: ['drive.google.com']
     }
 };
 
